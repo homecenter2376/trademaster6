b40991d94ffff3e2b2c701864c30bc4a3cb3694e
moonston001 <87723522+moonston001@users.noreply.github.com>
2025-03-27 05:52:18 +0530

    Fix: Update plugin URLs to use proxy and add CORS headers

--- next.config.mjs
+++ next.config.mjs
@@ -62,6 +62,16 @@ if (mode !== "export") {
         headers: CorsHeaders,
       },
       {
+        source: "/api/proxy/github/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "*" },
+          { key: "Access-Control-Allow-Headers", value: "*" },
+          { key: "Access-Control-Max-Age", value: "86400" }
+        ],
+      },
+      {
         source: "/:path*",
         headers: [
           {
